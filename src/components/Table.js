13aa069f3bd0ed36560d310b5c0a0921783262f1
Ahmed Hilles <57227022+Ahmed116@users.noreply.github.com>
2020-10-17 02:02:21 +0300

    final

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,16 +1,16 @@
 import React,{useState, useEffect} from 'react'
 import TicBlock from './TicBlock.js'
-
+import '../App.css'
 const Table = () => {
     const [table, setTable] = useState([0,0,0, 0,0,0, 0,0,0])
     const [winner, setWinner] = useState([])
-    const [last, setLast] = useState(Math.random() > 0.5? 5:3)
-    const [player1, setPlayer1] = useState('')
-    const [player2, setPlayer2] = useState('')
-    
+    const [last, setLast] = useState(Math.random() > 0.5 ? 5 : 3)
+    const [player1, setPlayer1] = useState({name: '', last: 5, count: 0})
+    const [player2, setPlayer2] = useState({name: '', last: 3, count: 0})
+
     useEffect(()=>{
       
-       
+        
         //checking winner row and col
         for (let i = 0; i <= 2; i++){
             const idx = (i % 3) * 3 // -> 0,3,6
@@ -40,36 +40,65 @@ const Table = () => {
         }
     }, [table])
 
+    const reset = () => {
+        // Clear all grids and winner message
+        setTable([0,0,0,0,0,0,0,0,0]);
+        setWinner([]);
+        setLast([])
+        setPlayer1({count:0})
+        setPlayer2({count:0})
+           }
+
+    const playAgain=() =>{
+        setTable([0,0,0,0,0,0,0,0,0]);
+        setWinner([]);
+        setLast([])
+    }
+
     const gameOver = () => {
             setTimeout(() => {
                 setTable([0,0,0,0,0,0,0,0,0]);
                 setWinner([]);
                 console.log('game over');
             },2000);
-            alert('winner is '+ winner)
-    } 
+        console.log(player1.last, 'player1.last')
+        console.log(last, 'last')
+        if (player1.last === last ) {
+            setPlayer1({...player1, count: player1.count + 1})
+            alert('winner is: ' + player1.name )
+        } else {
+            setPlayer2({...player2, count: player2.count + 1})
+            alert('winner is: ' + player2.name )
+
+        }
+    }
+
+    
+    
     console.log(winner);
     return (
     <div className='main-table'>
-        <h1> Tic Tac Toe </h1>
+        <h1 className="h1"> Tic Tac Toe </h1>
       <table className='table'>
       <tr>
-    <th>{player1}</th>
-    <th>{player2}</th>
+    <th>{player1.name ? (player1.name + ' Using ' + 'X') : 'Player1 Name'  }</th>
+    <th>{player2.name ? (player2.name + ' Using ' + 'O') : 'Player2 Name'}</th>
      </tr>
   <tr>
-    <td>0</td>
-    <td>0</td>
+    <td>{player1.count}</td>
+    <td>{player2.count}</td>
       </tr>
       </table>
-      <button onClick={() =>{
-            const player1 = prompt('x player name');
-            setPlayer1(player1);
+      <div className='addBtn'>
+      <button className="playerbtn" onClick={() =>{
+            const name = prompt('x player name');
+            setPlayer1( {...player1, name });
       }}>Add player 1</button>
-      <button onClick={() =>{
-           const player2 = prompt('0 player name');
-           setPlayer2(player2);
+      <button className="playerbtn" onClick={() =>{
+           const name = prompt('0 player name');
+          setPlayer2( {...player2, name });
       }}>Add player 2</button>
+      </div>
         <div className='row'>
             <TicBlock number={0} last={[last,setLast]} table={[table,setTable]} winner={winner}/> 
             <TicBlock number={1} last={[last,setLast]} table={[table,setTable]} winner={winner}/> 
@@ -85,7 +114,10 @@ const Table = () => {
             <TicBlock number={7} last={[last,setLast]} table={[table,setTable]} winner={winner}/> 
             <TicBlock number={8} last={[last,setLast]} table={[table,setTable]} winner={winner}/> 
         </div>
-
+        <div className='lastBtn'>
+        <button className="again" onClick={playAgain}>Play again</button>
+        <button className="again" onClick={reset}>Reset Result</button>
+        </div>
     </div>
     )
 }
